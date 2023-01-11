68d537bc51c056652868b7f14e105f000bea9373
Carl <carlfearby@me.com>
2023-01-11 18:45:20 +0000

    upgrade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes
 // , { useHash: true }  <==- for Electron refresh to work
-, { relativeLinkResolution: 'legacy' })
+, {})
   ],
   exports: [RouterModule]
 })
